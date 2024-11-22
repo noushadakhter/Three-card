403fda1c82b7aa45768bcd4f467dbe22934db988
noushadakhter <163678090+noushadakhter@users.noreply.github.com>
2024-11-22 23:20:42 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,60 @@
+export default function Home() {
+  return (
+    <div>
+      <nav className=" flex w-full bg-purple-300 h-32 items-center">
+      <div className="bg-green-300 h-10 w-96">Home</div>
+     
+     <div className="flex justify-end w-3/5 items-center">
+      
+      <div className="bg-red-600 h-10 w-44">Apply</div>
+      <div className="bg-yellow-300 h-10 w-44">Jobs</div>
+      <div className="bg-blue-600 h-10 w-44">Courses</div>
+
+     </div>
+     
+     
+      </nav>
+     
+
+      <div>
+      <h3 className="text-blue-700 font-bold text-center">
+      Certified Cloud Applied Generative AI Engineer (GenEng) and Solopreneur Developing Billion-Dollar Valued Developers and 
+      Solopreneurs.
+
+      </h3>
+     
+     
+      <p className="text-left">
+      The pace of technological change is accelerating, big players like Microsoft, Amazon, Google, and
+      OpenAI are winning by providing infrastructure, large AI foundation models, frameworks, 3D Metaverse experiences, 
+      and massive distribution networks.
+
+
+      </p>
+    </div> 
+
+
+
+    <nav className=" flex items-center">
+       <div className="w-72 bg-orange-200 h-40"></div>
+       <div className="w-72 bg-green-200 h-40"></div>
+       <div className="w-72 bg-red-500 h-40"></div>
+    </nav>
+
+    <div>
+      <h1 className="text-blue-700 font-bold">Core Courses Sequence
+      </h1>
+    </div>
+
+   <nav className=" flex items-center">
+   <div className="w-96 bg-green-700 h-52"></div>
+   <div className="w-96 bg-pink-400 h-52"></div>
+   </nav>
+
+
+            
+      
+    </div>
+
+   )
+}
